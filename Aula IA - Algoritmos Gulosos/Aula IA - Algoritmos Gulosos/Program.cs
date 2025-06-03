0cec183d9e50c4e68d0a0dc6e408d292dd0695cf
Pedro Henrique Santos <132759436+ph-santos0@users.noreply.github.com>
2025-06-03 20:54:03 -0300

    Update Program.cs

--- Aula IA - Algoritmos Gulosos/Aula IA - Algoritmos Gulosos/Program.cs
+++ Aula IA - Algoritmos Gulosos/Aula IA - Algoritmos Gulosos/Program.cs
@@ -15,45 +15,35 @@ namespace Aula_IA___Algoritmos_Gulosos
             Console.Write("Digite o valor do troco: ");
             double troco = double.Parse(Console.ReadLine());
 
-            while (troco > 0)
+            while (troco >= 0.01)
             {
-
-                if (troco > 0)
+                if (troco >= 0.20)
                 {
-                    Math.Round(troco = troco - 0.20);
+                    troco = Math.Round(troco - 0.20, 2);
                     cent20++;
-                    Console.WriteLine("entrou");
                 }
-                if (troco >= 0.11)
+                else if (troco >= 0.11)
                 {
-                    Math.Round(troco = troco - 0.11);
+                    troco = Math.Round(troco - 0.11, 2);
                     cent11++;
-                    Console.WriteLine("entrou");
                 }
-                if (troco >= 0.05)
+                else if (troco >= 0.05)
                 {
-                    Math.Round(troco = troco - 0.05);
+                    troco = Math.Round(troco - 0.05, 2);
                     cent5++;
-                    Console.WriteLine("entrou");
                 }
-                if (troco >= 0.01)
+                else if (troco >= 0.01)
                 {
-                    Math.Round(troco = troco - 0.01);
+                    troco = Math.Round(troco - 0.01, 2);
                     cent1++;
-                    Console.WriteLine("entrou");
-                }
-                else
-                {
-                    break;
                 }
             }
 
-
-            Console.WriteLine(cent20 + " " + cent11 + " " + cent5 + " " + cent1);
-
-
-
-
+            Console.WriteLine("Solução Gulosa:");
+            Console.WriteLine("Moedas de 20 centavos: " + cent20);
+            Console.WriteLine("Moedas de 11 centavos: " + cent11);
+            Console.WriteLine("Moedas de 5 centavos: " + cent5);
+            Console.WriteLine("Moedas de 1 centavo: " + cent1);
         }
     }
 }
