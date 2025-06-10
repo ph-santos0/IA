e4cacef63e67e6e9b26eeb0e2ff90cfc01909cbe
Pedro Henrique Santos <132759436+ph-santos0@users.noreply.github.com>
2025-06-10 08:38:37 -0300

    Update Program.cs

--- Atividade 4 -  Algoritmo Guloso com Algoritmos Genéticos/Atividade 4 -  Algoritmo Guloso com Algoritmos Genéticos/Program.cs
+++ Atividade 4 -  Algoritmo Guloso com Algoritmos Genéticos/Atividade 4 -  Algoritmo Guloso com Algoritmos Genéticos/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Atividade_4___Algoritmo_Guloso_com_Algoritmos_Genéticos
 {
@@ -14,48 +14,68 @@ namespace Atividade_4___Algoritmo_Guloso_com_Algoritmos_Genéticos
             Random rand = new Random();
             int tamanhoPopulacao = 100;
             int numeroGeracoes = 500;
+            double taxaMutacao = 0.1;
 
-            double melhorDiferenca = double.MaxValue;
-            int melhorTotalMoedas = int.MaxValue;
+            int[,] populacao = new int[tamanhoPopulacao, 4];
+
+            double menorCusto = double.MaxValue;
+
             int melhor20 = 0, melhor11 = 0, melhor5 = 0, melhor1 = 0;
+
+            for (int i = 0; i < tamanhoPopulacao; i++)
+            {
+                populacao[i, 0] = rand.Next(0, (int)(troco / 0.20) + 2);
+                populacao[i, 1] = rand.Next(0, (int)(troco / 0.11) + 2);
+                populacao[i, 2] = rand.Next(0, (int)(troco / 0.05) + 2);
+                populacao[i, 3] = rand.Next(0, (int)(troco / 0.01) + 2);
+            }
 
             for (int geracao = 0; geracao < numeroGeracoes; geracao++)
             {
-                for (int individuo = 0; individuo < tamanhoPopulacao; individuo++)
+                for (int i = 0; i < tamanhoPopulacao; i++)
                 {
-                    int qtd20 = rand.Next(0, (int)(troco / 0.20) + 1);
-                    int qtd11 = rand.Next(0, (int)(troco / 0.11) + 1);
-                    int qtd5 = rand.Next(0, (int)(troco / 0.05) + 1);
-                    int qtd1 = rand.Next(0, (int)(troco / 0.01) + 1);
+                    double totalValorMoedas = populacao[i, 0] * 0.20 + populacao[i, 1] * 0.11 + populacao[i, 2] * 0.05 + populacao[i, 3] * 0.01;
+                    totalValorMoedas = Math.Round(totalValorMoedas, 2);
+                    int totalMoedasUsadas = populacao[i, 0] + populacao[i, 1] + populacao[i, 2] + populacao[i, 3];
 
-                    double totalMoedas = qtd20 * 0.20 + qtd11 * 0.11 + qtd5 * 0.05 + qtd1 * 0.01;
-                    totalMoedas = Math.Round(totalMoedas, 2);
+                    double diferenca = Math.Abs(troco - totalValorMoedas);
+                    double custoAtual = diferenca + (totalMoedasUsadas * 0.0001);
 
-                    if (totalMoedas <= troco)
+                    if (custoAtual < menorCusto)
                     {
-                        double diferenca = troco - totalMoedas;
-                        int totalMoedasUsadas = qtd20 + qtd11 + qtd5 + qtd1;
+                        menorCusto = custoAtual;
+                        melhor20 = populacao[i, 0];
+                        melhor11 = populacao[i, 1];
+                        melhor5 = populacao[i, 2];
+                        melhor1 = populacao[i, 3];
+                    }
+                }
 
-                        if (diferenca < melhorDiferenca)
+                int[,] novaPopulacao = new int[tamanhoPopulacao, 4];
+
+                novaPopulacao[0, 0] = melhor20;
+                novaPopulacao[0, 1] = melhor11;
+                novaPopulacao[0, 2] = melhor5;
+                novaPopulacao[0, 3] = melhor1;
+
+                for (int i = 1; i < tamanhoPopulacao; i++)
+                {
+                    int pai1 = rand.Next(tamanhoPopulacao);
+                    int pai2 = rand.Next(tamanhoPopulacao);
+
+                    for (int g = 0; g < 4; g++)
+                    {
+                        novaPopulacao[i, g] = rand.NextDouble() < 0.5 ? populacao[pai1, g] : populacao[pai2, g];
+
+                        if (rand.NextDouble() < taxaMutacao)
                         {
-                            melhorDiferenca = diferenca;
-                            melhorTotalMoedas = totalMoedasUsadas;
-                            melhor20 = qtd20;
-                            melhor11 = qtd11;
-                            melhor5 = qtd5;
-                            melhor1 = qtd1;
-                        }
-                        else if (diferenca == melhorDiferenca && totalMoedasUsadas < melhorTotalMoedas)
-                        {
-                            melhorDiferenca = diferenca;
-                            melhorTotalMoedas = totalMoedasUsadas;
-                            melhor20 = qtd20;
-                            melhor11 = qtd11;
-                            melhor5 = qtd5;
-                            melhor1 = qtd1;
+                            int ajuste = rand.Next(-1, 2);
+                            novaPopulacao[i, g] = Math.Max(0, novaPopulacao[i, g] + ajuste);
                         }
                     }
                 }
+
+                populacao = novaPopulacao;
             }
 
             Console.WriteLine("\n--- Melhor solução encontrada ---");
@@ -65,6 +85,8 @@ namespace Atividade_4___Algoritmo_Guloso_com_Algoritmos_Genéticos
             Console.WriteLine($"Moedas de 1 centavo: {melhor1}");
 
             double totalFinal = melhor20 * 0.20 + melhor11 * 0.11 + melhor5 * 0.05 + melhor1 * 0.01;
+            int melhorTotalMoedas = melhor20 + melhor11 + melhor5 + melhor1;
+
             Console.WriteLine($"Total em moedas: R$ {Math.Round(totalFinal, 2)}");
             Console.WriteLine($"Total de moedas usadas: {melhorTotalMoedas}");
         }
